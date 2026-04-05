88f87e3fd4a32210caeaaeda20d194f70e599040
Hader Renteria <haderrenteria13az@gmail.com>
2026-04-05 10:23:25 -0500

    feat: temporarly add run-seed endpoint to configure azure database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { execSync } from 'child_process';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get('run-seed')
+  runSeed(@Res() res: Response) {
+    try {
+      const output = execSync('node dist/prisma/seed.js').toString();
+      return res.status(200).send(`<pre>Seed ejecutado con exito:\n${output}</pre>`);
+    } catch (error) {
+      return res.status(500).send(`<pre>Error ejecutando seed:\n${error.message}\n${error.stdout?.toString()}</pre>`);
+    }
+  }
 
   @Get()
   getHello(): object {
